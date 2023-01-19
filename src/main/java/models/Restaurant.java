ab8bb6d8641bd2bdde2308f4eb067e2292aba01a
kajela <titoyut56@gmail.com>
2023-01-19 04:58:46 -0500

    add the initial files

--- src/main/java/models/Restaurant.java
+++ src/main/java/models/Restaurant.java
@@ -48,4 +48,20 @@ public class Restaurant {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
 }
--- src/test/java/models/RestaurantTest.java
+++ src/test/java/models/RestaurantTest.java
@@ -7,12 +7,22 @@ import static org.junit.Assert.assertEquals;
 public class RestaurantTest {
     @Test
     public void RestaurantInstantiatesCorrectly_true(){
-        Restaurant restaurant = new Restaurant("hotel","","","");
+        Restaurant restaurant = new Restaurant("hotel","1","1","1");
         assertEquals(true, restaurant instanceof Restaurant);
     }
     @Test
     public void RestaurantInstantiatesWithName(){
-        Restaurant restaurant =new Restaurant("hotel","","","");
+        Restaurant restaurant =new Restaurant("hotel","1","1","1");
         assertEquals( "hotel",restaurant.getName());
+    }
+    @Test
+    public void RestaurantInstantiatesWithAddress(){
+    Restaurant restaurant = new Restaurant("hotel","1","1","1");
+    assertEquals("1", restaurant.getAddress());
+    }
+    @Test
+    public void RestaurantInstantiatesWithZipcode(){
+        Restaurant restaurant = new Restaurant("hotel","1","1","1");
+        assertEquals("1",restaurant.getZipcode());
     }
 }
