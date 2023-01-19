0a4b77e3edea4c1081d1687b6db6343ed584c3c0
kajela <titoyut56@gmail.com>
2023-01-19 10:05:16 -0500

    add the initial files

--- src/test/java/models/RestaurantTest.java
+++ src/test/java/models/RestaurantTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class RestaurantTest {
   @Before
@@ -44,6 +45,42 @@ public class RestaurantTest {
     public void getEmailReturnsCorrectEmail() throws Exception{
     Restaurant restaurant = setupRestaurant();
     assertEquals("titoyut56@gmail.com", restaurant.getEmail());
+    }
+    @Test
+    public void setNameSetsNameCorrectly() throws Exception{
+    Restaurant restaurant = setupRestaurant();
+    restaurant.setName("crystalust");
+    assertNotEquals("thisa", restaurant.getName());
+    }
+    @Test
+    public void setAddressSetsAddressCorrectly() throws Exception{
+    Restaurant restaurant = setupRestaurant();
+    restaurant.setAddress("mundoro");
+    assertNotEquals("zimmer", restaurant.getAddress());
+    }
+    @Test
+    public void setZipcodeCorrectlySetsZipcode() throws Exception{
+    Restaurant restaurant = setupRestaurant();
+    restaurant.setZipcode("120");
+    assertNotEquals("130", restaurant.getZipcode());
+    }
+    @Test
+    public void setPhoneCorrectlySetsPhone() throws Exception{
+    Restaurant restaurant = setupRestaurant();
+    restaurant.setPhone("0717553340");
+    assertNotEquals("0776509158", restaurant.getPhone());
+    }
+    @Test
+    public void setWebsiteCorrectlySetsWebsite() throws Exception{
+    Restaurant restaurant = setupRestaurant();
+    restaurant.setWebsite("http//kca.com");
+    assertNotEquals("http//kajela.com", restaurant.getWebsite());
+    }
+    @Test
+    public void setEmailCorrectlySetsEmail() throws Exception{
+    Restaurant restaurant = setupRestaurant();
+    restaurant.setEmail("titoyut56@gmail.com");
+    assertNotEquals("wambuchiri12@gmail.com", restaurant.getEmail());
     }
 
   public Restaurant setupRestaurant (){
