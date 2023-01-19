24727610bb5546209962b08c37ec62d0aa737c37
kajela <titoyut56@gmail.com>
2023-01-19 14:59:33 -0500

    add the initial files

--- src/test/java/models/ReviewTest.java
+++ src/test/java/models/ReviewTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ReviewTest {
     @Before
@@ -34,6 +35,30 @@ public class ReviewTest {
     public void getRestaurantIdReturnsCorrectRestaurantId() throws Exception{
         Review review =setupReview();
         assertEquals(2, review.getRestaurantId());
+    }
+    @Test
+    public void setContentCorrectlySetsContent() throws Exception{
+        Review review = setupReview();
+        review.setContent("myn");
+        assertNotEquals("kajela", review.getContent());
+    }
+    @Test
+    public void setWrittenByCorrectlySetsWrittenBy() throws Exception{
+        Review review = setupReview();
+        review.setWrittenBy("tito");
+        assertNotEquals("muchoki", review.getWrittenBy());
+    }
+    @Test
+    public void setRatingCorrectlySetsRating() throws Exception{
+        Review review = setupReview();
+        review.setRating(59);
+        assertNotEquals(60, review.getRating());
+    }
+    @Test
+    public void setRestaurantIdCorrectlySetsRestaurantsId() throws Exception{
+        Review review = setupReview();
+        review.setRestaurantId(23);
+        assertNotEquals(20, review.getRestaurantId());
     }
 
     public Review setupReview(){
