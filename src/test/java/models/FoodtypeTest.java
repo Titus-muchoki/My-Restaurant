d9f757b095d80c0278a7c2db46a60b1880713ef2
kajela <titoyut56@gmail.com>
2023-01-20 03:29:17 -0500

    add the initial files

--- src/test/java/models/FoodtypeTest.java
+++ src/test/java/models/FoodtypeTest.java
@@ -20,6 +20,19 @@ public class FoodtypeTest {
         Foodtype foodtype = setupFoodtype();
         assertEquals("tito", foodtype.getName());
     }
+    @Test
+    public void setIdCorrectlySetsTheId() throws Exception{
+        Foodtype foodtype = setupFoodtype();
+        foodtype.setName("vima");
+        assertEquals("vima", foodtype.getName());
+
+    }
+    @Test
+    public void setIdSetsIdCorrectly() throws Exception{
+        Foodtype foodtype = setupFoodtype();
+        foodtype.setId(2);
+        assertEquals(2, foodtype.getId());
+    }
     public Foodtype setupFoodtype(){
         return new Foodtype("tito");
     }
